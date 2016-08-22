b41d9f52bcd0d698846774afd4b50a8e8053a51c
XhrHome <269175226@qq.com>
2016-08-22 23:57:36 +0800

    解决框架多一个滚动条的bug

--- LIMS/Scripts/outlook.js
+++ LIMS/Scripts/outlook.js
@@ -152,7 +152,7 @@ function addTab(subtitle, url, icon) {
 }
 
 function createFrame(url) {
-    var s = '<iframe scrolling="auto" frameborder="0"  src="' + url + '" style="width:100%;height:100%;"></iframe>';
+    var s = '<iframe scrolling="auto" frameborder="0"  src="' + url + '" style="width:100%;height:99%;"></iframe>';
     return s;
 }
 
