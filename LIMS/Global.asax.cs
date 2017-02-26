a0c09db688a9f54e24913c57b883fbc627036324
Xing <Xing@XING-PC>
2017-02-26 22:48:37 +0800

    添加主程序注释

--- LIMS/Global.asax.cs
+++ LIMS/Global.asax.cs
@@ -12,9 +12,13 @@ namespace LIMS
     {
         protected void Application_Start()
         {
+            //注册 ASP.NET MVC 应用程序中的所有区域
             AreaRegistration.RegisterAllAreas();
+            //注册 全局的Filters
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            //注册 路由规则
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //注册 打包绑定（js,css等）
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
